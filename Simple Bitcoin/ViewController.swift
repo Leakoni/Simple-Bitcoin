78df1108b9a018235df7085d0c65f47c831e64c4
Leakoni <dovydas.bumblauskas@aol.com>
2021-08-04 19:40:32 +0200

    Initial Commit

---
+++ Simple Bitcoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Bitcoin
+//
+//  Created by Dovydas Bumblauskas on 2021-08-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
