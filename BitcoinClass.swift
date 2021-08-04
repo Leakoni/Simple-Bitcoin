0d07d40ec9fb5d89f042b55ddac38e41a8f5e9f7
Leakoni <dovydas.bumblauskas@aol.com>
2021-08-04 19:54:54 +0200

    API Test V1

---
+++ BitcoinClass.swift
@@ -0,0 +1,116 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try Welcome(json)
+
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let data: DataClass
+    let timestamp: Int
+}
+
+// MARK: Welcome convenience initializers and mutators
+
+extension Welcome {
+    init(data: Data) throws {
+        self = try newJSONDecoder().decode(Welcome.self, from: data)
+    }
+
+    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+        guard let data = json.data(using: encoding) else {
+            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+        }
+        try self.init(data: data)
+    }
+
+    init(fromURL url: URL) throws {
+        try self.init(data: try Data(contentsOf: url))
+    }
+
+    func with(
+        data: DataClass? = nil,
+        timestamp: Int? = nil
+    ) -> Welcome {
+        return Welcome(
+            data: data ?? self.data,
+            timestamp: timestamp ?? self.timestamp
+        )
+    }
+
+    func jsonData() throws -> Data {
+        return try newJSONEncoder().encode(self)
+    }
+
+    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
+        return String(data: try self.jsonData(), encoding: encoding)
+    }
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let id, symbol, currencySymbol, type: String
+    let rateUsd: String
+}
+
+// MARK: DataClass convenience initializers and mutators
+
+extension DataClass {
+    init(data: Data) throws {
+        self = try newJSONDecoder().decode(DataClass.self, from: data)
+    }
+
+    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+        guard let data = json.data(using: encoding) else {
+            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+        }
+        try self.init(data: data)
+    }
+
+    init(fromURL url: URL) throws {
+        try self.init(data: try Data(contentsOf: url))
+    }
+
+    func with(
+        id: String? = nil,
+        symbol: String? = nil,
+        currencySymbol: String? = nil,
+        type: String? = nil,
+        rateUsd: String? = nil
+    ) -> DataClass {
+        return DataClass(
+            id: id ?? self.id,
+            symbol: symbol ?? self.symbol,
+            currencySymbol: currencySymbol ?? self.currencySymbol,
+            type: type ?? self.type,
+            rateUsd: rateUsd ?? self.rateUsd
+        )
+    }
+
+    func jsonData() throws -> Data {
+        return try newJSONEncoder().encode(self)
+    }
+
+    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
+        return String(data: try self.jsonData(), encoding: encoding)
+    }
+}
+
+// MARK: - Helper functions for creating encoders and decoders
+
+func newJSONDecoder() -> JSONDecoder {
+    let decoder = JSONDecoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    return decoder
+}
+
+func newJSONEncoder() -> JSONEncoder {
+    let encoder = JSONEncoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        encoder.dateEncodingStrategy = .iso8601
+    }
+    return encoder
+}
--- Simple Bitcoin/ViewController.swift
+++ Simple Bitcoin/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var testing: UITextView!
+    
+    var url = "https://api.coincap.io/v2/rates/bitcoin"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        jss()
     }
 
+    
+    func jss(){
+        if let url = URL(string: url) {
+           URLSession.shared.dataTask(with: url) { data, response, error in
+              if let data = data {
+                 if let jsonString = String(data: data, encoding: .utf8) {
+
+                    let welcome = try! Welcome(jsonString)
+                    print(welcome.data.rateUsd)
+                    
+                 }
+               }
+           }.resume()
+        }
+    }
 
 }
 
